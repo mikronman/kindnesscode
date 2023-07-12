1f68a0c389df72f94434801d84969ec2b37ae9b8
mikronman <mkleczkajr@gmail.com>
2023-07-12 19:06:08 -0400

    forgot to route subscribe. Silly me

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { BennyComponent } from './components/benny/benny.component';
+import { SubscribeComponent } from './components/subscribe/subscribe.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: 'welcome', component: WelcomeComponent },
-  { path: 'benny', component: BennyComponent }
+  { path: 'benny', component: BennyComponent },
+  { path: 'subscribe', component: SubscribeComponent }
 ];
 
 @NgModule({
