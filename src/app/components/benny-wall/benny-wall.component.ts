367ac25694b1b731ce046a890fcd55bdc73665f4
mikronman <mkleczkajr@gmail.com>
2023-07-12 21:50:54 -0400

    quick refactor on the picture list in the ts... made it a loop for when I add more pictures.

--- src/app/components/benny-wall/benny-wall.component.ts
+++ src/app/components/benny-wall/benny-wall.component.ts
@@ -6,33 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./benny-wall.component.scss']
 })
 export class BennyWallComponent {
-  pictures = [
-    { url: 'benny_1.png' },
-    { url: 'benny_2.png' },
-    { url: 'benny_3.png' },
-    { url: 'benny_4.png' },
-    { url: 'benny_5.png' },
-    { url: 'benny_6.png' },
-    { url: 'benny_7.png' },
-    { url: 'benny_8.png' },
-    { url: 'benny_9.png' },
-    { url: 'benny_10.png' },
-    { url: 'benny_11.png' },
-    { url: 'benny_12.png' },
-    { url: 'benny_13.png' },
-    { url: 'benny_14.png' },
-    { url: 'benny_15.png' },
-    { url: 'benny_16.png' },
-    { url: 'benny_17.png' },
-    { url: 'benny_18.png' },
-    { url: 'benny_19.png' },
-    { url: 'benny_20.png' },
-    { url: 'benny_21.png' },
-    { url: 'benny_22.png' },
-    { url: 'benny_23.png' },
-    { url: 'benny_24.png' },
-    { url: 'benny_25.png' },
-    { url: 'benny_26.png' },
-    { url: 'benny_27.png' }
-  ];
+  pictures: { url: string }[] = [];
+
+  constructor() {
+    this.buildPictures();
+  }
+
+  private buildPictures(): void {
+    for (let i = 1; i <= 27; i++) {
+      const picture = { url: `benny_${i}.png` };
+      this.pictures.push(picture);
+    }
+  }
 }
