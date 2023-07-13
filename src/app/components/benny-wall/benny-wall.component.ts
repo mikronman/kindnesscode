178bab7ca6412cdf5c3788de126514f3d58a32af
mikronman <mkleczkajr@gmail.com>
2023-07-12 21:46:49 -0400

    added more photos

--- src/app/components/benny-wall/benny-wall.component.ts
+++ src/app/components/benny-wall/benny-wall.component.ts
@@ -22,6 +22,17 @@ export class BennyWallComponent {
     { url: 'benny_13.png' },
     { url: 'benny_14.png' },
     { url: 'benny_15.png' },
-    { url: 'benny_16.png' }
+    { url: 'benny_16.png' },
+    { url: 'benny_17.png' },
+    { url: 'benny_18.png' },
+    { url: 'benny_19.png' },
+    { url: 'benny_20.png' },
+    { url: 'benny_21.png' },
+    { url: 'benny_22.png' },
+    { url: 'benny_23.png' },
+    { url: 'benny_24.png' },
+    { url: 'benny_25.png' },
+    { url: 'benny_26.png' },
+    { url: 'benny_27.png' }
   ];
 }
