e04ca58b67a34f423bafef60836a35256e1513f6
mikronman <mkleczkajr@gmail.com>
2023-07-14 12:40:03 -0400

    forgot to update meta for benny page

--- src/app/components/benny/benny.component.ts
+++ src/app/components/benny/benny.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
-import { Meta } from '@angular/platform-browser';
 import { DOCUMENT } from '@angular/common';
+import { SeoService } from '../../seo.service';
 
 @Component({
   selector: 'app-benny',
@@ -11,27 +11,23 @@ import { DOCUMENT } from '@angular/common';
 export class BennyComponent implements OnInit, OnDestroy {
   constructor(
     private router: Router, 
-    private metaService: Meta,
-    @Inject(DOCUMENT) private document: Document
+    @Inject(DOCUMENT) private document: Document,
+    private seoService: SeoService
     ) {}
 
   ngOnInit() {
     const url = this.document.location.origin + this.router.url;
-    //console.log(url);
-    this.metaService.addTags([
-      {name: 'keywords', content: 'About Benny, Benny Obituary, Benny Memories'},
-      {name: 'description', content: `Learn a little bit more about the life of Benny, and see a wall of memories.`},
-      {property: 'og:title', content: 'About Benny'},
-      {property: 'og:description', content: `Learn a little bit more about the life of Benny, and see a wall of memories.`},
-      {property: 'og:url', content: url}
-    ]);
+      this.seoService.setTitle('Who is Benny?');
+      this.seoService.setMetaTags([
+        {name: 'keywords', content: 'Benny Kleczka, Who is Benny, Benny, Kindness'},
+        {name: 'description', content: `Learn more about who this organization is built upon, his spirit of kindness.`},
+        {property: 'og:title', content: 'Who is Benny?'},
+        {property: 'og:description', content: `Learn more about who this organization is built upon, his spirit of kindness.`},
+        {property: 'og:url', content: url}
+      ]);
   }
 
   ngOnDestroy () {
-    this.metaService.removeTag("name='keywords'");
-    this.metaService.removeTag("name='description'");
-    this.metaService.removeTag("property='og:title'");
-    this.metaService.removeTag("property='og:description'");
-    this.metaService.removeTag("property='og:url'");
+    this.seoService.clearMetaTags();
   }
 }
