5ed9eca909036afc38b585147240040336e54bc3
mikronman <mkleczkajr@gmail.com>
2023-07-14 17:50:03 -0400

    made the roadmap verbiage less aloof

--- src/app/components/roadmap/roadmap.component.ts
+++ src/app/components/roadmap/roadmap.component.ts
@@ -16,35 +16,36 @@ export class RoadmapComponent {
   roadmapItems: RoadmapItem[] = [
     {
       title: 'Engagement Strategies',
-      desc: 'We will continue developing our online presence via the website and social media. From there we will plan to establish awareness campaigns around digital ethics and empathy. Long term we envision greater community building and partnerships that synergize with our mission and vision.'
+      desc: `We're amping up our online game through our website and social media, and planning awareness campaigns about digital ethics and empathy. In the future, we're all about growing our community and making partnerships that mesh with our mission.`
     },
     {
       title: 'Program Development',
-      desc: `Our vision involves four key strategies. First, we aim to develop educational resources that merge coding with digital ethics. Second, we will address community needs, offering empathetic coding workshops and ethical digital behavior campaigns. Third, we're committed to forming strategic partnerships to amplify our impact. Lastly, through our advocacy, we inspire ethical behavior, encouraging kindness both online and offline.`
+      desc: `We've got four big strategies. One, creating educational resources that blend coding and digital ethics. Two, tackling community needs with coding workshops and campaigns promoting ethical digital behavior. Three, making partnerships to widen our reach and impact. Four, advocating for ethical behavior to inspire kindness both online and offline.`
     },
     {
       title:  'Business & Legal',
-      desc: 'We plan to file as a 501c3 entity, securing tax-exempt status. From there we can set up business accounts and begin integrating platforms to accept donations.'
+      desc: `We’re planning to register as a 501c3 entity to get tax-exempt status, which lets us set up business accounts and start getting donations.`
     },
     {
       title:  'Team Building',
-      desc: 'We will be working to fulfill initial key roles to get bigger initiatives off the ground. We will be working on a clear organizational structure as well as collaboration processes and tooling. Long term we will establish a board of directors and advisors, creating bylaws and governance policies.'
+      desc: `We’re in the process of filling important roles to get bigger projects started. We’re figuring out our organizational structure, how we’ll work together, and what tools we need. Later on, we'll establish a board of directors and advisors, and set up bylaws and governance policies.`
     },
     {
       title:  'Finances',
-      desc: 'As we grow we will need to establish sound financial planning and accounting. We will be orchestrating fundraising events, working for donations, grants and sponsorships.'
+      desc: `As we get bigger, we’ll be setting up solid financial planning and accounting. We’ll also be organizing fundraisers, and working towards getting donations, grants, and sponsorships.`
     },
     {
       title:  'Technology',
-      desc: 'We have a lot of work ahead of us in developing platforms to manage events, partnerships, volunteers and generous donors. We will need to establish a secure payment system to collect donations and provide transparent reporting and analytics. We will also need to build more internal services to manage the growing org.'
+      desc: `We’ve got a lot to do developing platforms to manage events, partnerships, volunteers, and generous donors. We need to set up a secure payment system for donations and transparent reporting and analytics. We’ll also build more services to manage our growing organization.`
     },
     {
       title:  'Data & Metrics',
-      desc: 'If successfully built and managed we expect to provide meaningful data around how we are using donations to impact society - data collection methods, impact framework and impact rubric.'
+      desc: `With all our efforts, we aim to provide meaningful data about how we use donations to make an impact. This involves figuring out how we’ll collect data, and developing our impact framework and rubric.`
     },
     {
       title:  'Innovation',
-      desc: `We aim to cultivate kindness in cyberspace, merging empathy principles with foundational programming knowledge. We're focused on devising an effective curriculum and a novel method for quantifying the social impact of each donation. Through innovative approaches, we strive to offer a unique experience to our communities.`
+      desc: `We're excited about bringing kindness into the world of cyberspace. Our goal is to create a curriculum that blends empathy with coding basics. We're also developing a new way to measure the social impact of each donation. We're confident these fresh approaches will offer a unique experience to our communities.`
     }
+    
   ];
 }
