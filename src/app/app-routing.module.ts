b26682eb6f108ff686f2c2dd98ae45047872ec4f
mikronman <mkleczkajr@gmail.com>
2023-07-14 17:56:44 -0400

    decided to move roadmap to it's own page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { BennyComponent } from './components/benny/benny.component';
 import { SubscribeComponent } from './components/subscribe/subscribe.component';
+import { RoadmapComponent } from './components/roadmap/roadmap.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'benny', component: BennyComponent },
-  { path: 'subscribe', component: SubscribeComponent }
+  { path: 'subscribe', component: SubscribeComponent },
+  { path: 'roadmap', component: RoadmapComponent }
 ];
 
 @NgModule({
