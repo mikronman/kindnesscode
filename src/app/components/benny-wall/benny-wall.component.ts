5f24e1c2f187c18abceb8d961839fbbb67672d3d
mikronman <mkleczkajr@gmail.com>
2023-07-18 22:18:23 -0400

    Added more pics of benny.

--- src/app/components/benny-wall/benny-wall.component.ts
+++ src/app/components/benny-wall/benny-wall.component.ts
@@ -7,7 +7,7 @@ import { Component, HostListener } from '@angular/core';
 })
 export class BennyWallComponent {
   pictures: { url: string, loaded: boolean }[] = [];
-  totalPictures = 59;
+  totalPictures = 71;
   batchSize = 10;
   currentBatchIndex = 0;
 
